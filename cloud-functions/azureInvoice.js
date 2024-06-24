8814654e079ff39bb69e684bdf0eb49c91930588
Ryan McGuire <ryanmcguire@berkeley.edu>
2024-06-23 19:41:07 -0500

    batching code broken with many simultaneous requests

--- cloud-functions/azureInvoice.js
+++ cloud-functions/azureInvoice.js
@@ -56,42 +56,42 @@ function formatFields(fields) {
 }
 
 module.exports = async function extractFields(fileContent) {
-  const endpoint = "https://centralus.api.cognitive.microsoft.com/";
-  const key = process.env.AZURE_FORM_RECOGNIZER_KEY;
+  // const endpoint = "https://centralus.api.cognitive.microsoft.com/";
+  // const key = process.env.AZURE_FORM_RECOGNIZER_KEY;
 
-  if (!fileContent) {
-    console.error("No file content for Azure Form Recognizer to analyze");
-  } else if (!key || !endpoint) {
-    console.error("Azure Form Recognizer credentials not configured");
-  }
+  // if (!fileContent) {
+  //   console.error("No file content for Azure Form Recognizer to analyze");
+  // } else if (!key || !endpoint) {
+  //   console.error("Azure Form Recognizer credentials not configured");
+  // }
 
-  try {
-    const client = new DocumentAnalysisClient(
-      endpoint,
-      new AzureKeyCredential(key)
-    );
-    const poller = await client.beginAnalyzeDocument(
-      "prebuilt-invoice",
-      fileContent
-    );
-    const {
-      pages, // pages extracted from the document, which contain lines and words
-      tables, // extracted tables, organized into cells that contain their contents
-      styles, // text styles (ex. handwriting) that were observed in the document
-      keyValuePairs, // extracted pairs of elements  (directed associations from one element in the input to another)
-      documents, // extracted fields from the document
-    } = await poller.pollUntilDone();
+  // try {
+  //   const client = new DocumentAnalysisClient(
+  //     endpoint,
+  //     new AzureKeyCredential(key)
+  //   );
+  //   const poller = await client.beginAnalyzeDocument(
+  //     "prebuilt-invoice",
+  //     fileContent
+  //   );
+  //   const {
+  //     pages, // pages extracted from the document, which contain lines and words
+  //     tables, // extracted tables, organized into cells that contain their contents
+  //     styles, // text styles (ex. handwriting) that were observed in the document
+  //     keyValuePairs, // extracted pairs of elements  (directed associations from one element in the input to another)
+  //     documents, // extracted fields from the document
+  //   } = await poller.pollUntilDone();
 
-    if (!documents || documents.length === 0) {
-      console.error("Azure found no document");
-    } else if (documents.length > 1) {
-      console.error("Azure found more than 1 document");
-    }
+  //   if (!documents || documents.length === 0) {
+  //     console.error("Azure found no document");
+  //   } else if (documents.length > 1) {
+  //     console.error("Azure found more than 1 document");
+  //   }
 
-    const { fields } = documents[0];
-    return formatFields(fields);
-  } catch (error) {
-    console.error(error);
-  }
+  //   const { fields } = documents[0];
+  //   return formatFields(fields);
+  // } catch (error) {
+  //   console.error(error);
+  // }
   return null;
 };
