7bb3721a43fb63365cd4610884258910ac5ef00e
Ryan McGuire <ryanmcguire@berkeley.edu>
2024-07-03 14:58:05 -0500

    fix bug in batches index creation that was causing some indices to be missed, add index creation for documentsFields

--- next-js-app/testing/createFirestoreIndices.js
+++ next-js-app/testing/createFirestoreIndices.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../.env.local" });
+require("dotenv").config({ path: ".env.local" });
 const readline = require("readline");
 const { Firestore, Timestamp } = require("@google-cloud/firestore");
 
@@ -18,16 +18,24 @@ const combinations = (fields) => {
   return fields.reduce((a, v) => a.concat(a.map((r) => [v, ...r])), [[]]);
 };
 
-const organization = "utexas";
-const fields = [
-  "createdFrom",
-  "createdTo",
-  "isCheckedOut",
-  "isFinished",
-  "isFull",
-];
+const promptUserToContinue = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-const testCombination = async (combination) => {
+  return new Promise((resolve) => {
+    rl.question(
+      "An error occurred. Please add the missing index and press Enter to continue...",
+      () => {
+        rl.close();
+        resolve();
+      }
+    );
+  });
+};
+
+const testBatchCombination = async (combination) => {
   let query = firestore
     .collection("batches")
     .where("organization", "==", organization);
@@ -52,32 +60,72 @@ const testCombination = async (combination) => {
   }
 };
 
-const promptUserToContinue = () => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const testDocumentCombination = async (combination) => {
+  let query = firestore
+    .collection("documents")
+    .where("organization", "==", organization);
 
-  return new Promise((resolve) => {
-    rl.question(
-      "An error occurred. Please add the missing index and press Enter to continue...",
-      () => {
-        rl.close();
-        resolve();
-      }
-    );
-  });
+  if (combination.includes("createdFromDate"))
+    query = query.where("timeCreated", ">=", Timestamp.fromDate(new Date()));
+  if (combination.includes("createdToDate"))
+    query = query.where("timeCreated", "<=", Timestamp.fromDate(new Date()));
+  if (combination.includes("filename"))
+    query = query.where("filename", "==", "test.pdf");
+  if (combination.includes("reviewed"))
+    query = query.where("reviewed", "==", false);
+  if (combination.includes("offset")) query = query.offset(0);
+  if (combination.includes("limit")) query = query.limit(10);
+  try {
+    const snapshot = await query.get();
+    const documents = snapshot.docs.map((doc) => doc.data());
+
+    return documents.length;
+  } catch (error) {
+    console.error(`Failed for combination: ${combination}`, error);
+    await promptUserToContinue();
+  }
 };
 
-const runTests = async () => {
-  const allCombinations = combinations(fields);
+const createBatchesIndices = async () => {
+  const allCombinations = combinations(batchesFields);
 
   let i = 1;
   for (const combination of allCombinations) {
-    const numBatches = await testCombination(combination);
+    const numBatches = await testBatchCombination(combination);
     console.log(`${i}. ${combination}: ${numBatches} batches found.`);
     i++;
   }
 };
 
-runTests();
+const createDocumentsIndices = async () => {
+  const allCombinations = combinations(documentsFields);
+
+  let i = 1;
+  for (const combination of allCombinations) {
+    const numDocuments = await testDocumentCombination(combination);
+    console.log(`${i}. ${combination}: ${numDocuments} documents found.`);
+    i++;
+  }
+};
+
+const organization = "utexas";
+
+const batchesFields = [
+  "createdFromDate",
+  "createdToDate",
+  "isCheckedOut",
+  "isFinished",
+  "isFull",
+];
+
+const documentsFields = [
+  "createdFromDate",
+  "createdToDate",
+  "filename",
+  "reviewed",
+  "offset",
+  "limit",
+];
+
+createBatchesIndices();
+createDocumentsIndices();
